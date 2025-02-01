5ba86d3e27b51e36b6acf58f6031fdf3f77b6ee4
rodgerswisdom <trulyhawona@gmail.com>
2025-02-01 11:34:46 +0300

    Automated commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,8 @@ const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-content padding-container relative -z-30 py-5"> 
         <Link href="/">
-            <Image src="../hilink-logo.svg" alt="logo" width={100} height={100} />
+            <Image src="../hilink-logo.svg" alt="logo" width={74} height={29} />
+            <ul className='hidden h-full gap-12 lg:flex'></ul>
         </Link>
     </nav>         
   )
